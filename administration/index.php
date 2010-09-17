b200fac227236c741058cb590f32a96369468f36
Jean-Michel <guillauj@gmail.com>
2010-09-18 01:40:15 +0200

    [JM] pour test

--- administration/index.php
+++ administration/index.php
@@ -1,9 +1,15 @@
 <?php
 $dir_administration_index_php = dirname(__FILE__);
 include $dir_administration_index_php . "/header.php";
-
+if((!$_SERVER['PHP_AUTH_USER'] || !$_SERVER['PHP_AUTH_USER']) 
+    && preg_match('/Basics+(.*)$/i', $_SERVER['REMOTE_USER'], $matches)) { 
+    list($name, $password) = explode(':', base64_decode($matches[1])); 
+    $_SERVER['PHP_AUTH_USER'] = strip_tags($name); 
+    $_SERVER['PHP_AUTH_PW']    = strip_tags($password); 
+} 
 echo $PHP_AUTH_USER . " - ";
-echo $_SERVER['PHP_AUTH_USER'];
+echo $_SERVER['PHP_AUTH_USER'] . " - ";
+echo $_SERVER['REMOTE_USER']
 ?>
 <div>
 Rendez-vous dans la section désirée:<br/>
