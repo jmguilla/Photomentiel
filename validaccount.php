ebc8641dda4108cb9f6a8f1f27f98a2c4be9fc7a
unknown <jlscheef@.(none)>
2010-09-21 16:51:42 +0200

    fix bugs, add limit in smart search, improve graphics

--- validaccount.php
+++ validaccount.php
@@ -22,13 +22,13 @@ if(!$activated){
 
 ?>
 <div id="full_content_top">
-		Votre Compte est maintenant activé !
+		Votre compte est maintenant activé !
 </div>
 <div id="full_content_mid">
 	<div class="separator10"></div>
 	<div id="content_validate">
 		<div class="separator10" style="height:140px;"></div>
-		Votre compte a été validé avec succés, nous vous souhaitons la bienvenue parmi nous.<br/>
+		Votre compte a été activé avec succés, nous vous souhaitons la bienvenue parmi nous.<br/>
 		Vous pouvez maintenant vous identifier en utilisant les champs ci-dessus.<br/>
 		<input id="gohome" type="button" class="button" value="Retour Accueil" onClick="document.location.href='index.php'"></input>
 		<div class="separator10" style="height:150px;"></div>
