34f4e6b6f5e24f76c16ec3d6a8c0af9c737062db
unknown <jlscheef@.(none)>
2010-09-28 10:51:08 +0200

    update cleandb.sql

--- config/clean_bd.sql
+++ config/clean_bd.sql
@@ -1,12 +1,3 @@
-drop table Photographe;
-drop table Utilisateur;
-drop table Adresse;
-drop table TaillePapier;
-drop table TypePapier;
-drop table Couleur;
-drop table Album;
-drop table Evenement;
-drop table CommandePhoto;
-drop table Commande;
-drop table PrixTaillePapierAlbum;
-drop table StringID;
+drop database photomentiel;
+create database photomentiel;
+connect photomentiel;
