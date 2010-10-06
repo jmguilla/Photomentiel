ae9cb93ed5086d171191173ed2df657d7f83a145
Jean-Michel <guillauj@gmail.com>
2010-10-06 18:46:35 +0200

    adjout/suppression module bancaire arnaud

--- e-transactions/selectcard.php
+++ e-transactions/selectcard.php
@@ -2,7 +2,7 @@
 //File to be imported when selecting credit card
 function displayCards($p_module, $p_amount, $p_transactionID, $p_userID, $p_albumID){
 	//TODO change SIREN value when available
-	$SIREN_A  = '000000000';
+	$SIREN_A  = '523919066';
 	$SIREN_JM = '521000018';
 	if ($p_module == $SIREN_A){
 		//Arnaud
