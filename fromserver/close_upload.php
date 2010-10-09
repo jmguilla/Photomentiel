6f8e2cf29a2917a136e14762f1d4cc7ffbea6698
unknown <jl@.(none)>
2010-10-09 17:06:10 +0200

    add request to close transfert

---
+++ fromserver/close_upload.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * uploadComplete.php is just the request to close pictures upload comming from storage server
+ * 
+ * Author : SCHIOUFF (All rights reserved)
+ * 
+ * Created on 9 oct. 2010
+ * Version : 1.0.0
+ * Since : 1.0.0
+ *
+ */
+ 
+include_once("../classes/modele/StringID.class.php");
+include_once("../classes/modele/Album.class.php");
+
+if (!isset($_GET['stringID'])){
+	echo 1;
+	exit;
+}
+$sidObj = StringID::getStringIDDepuisID($_GET['stringID']);
+if (!$sidObj){
+	echo 2;
+	exit;
+}
+$albumObj = Album::getAlbumDepuisID($sidObj->getID_Album());
+if (!$albumObj){
+	echo 3;
+	exit;
+}
+$retCode = $albumObj->finirTransfert();
+if (!$retCode){
+	echo 4;
+	exit;
+}
+echo 0;
+
+?>
