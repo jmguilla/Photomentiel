4842bbbe981f68a72b955f10420d703384d9f1e4
jmg <guillauj@gmail.com>
2010-10-13 19:02:40 +0200

    cleanup

--- classes/controleur/test-create-error.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dir_test_create_error_php = dirname(__FILE__);
-include_once $dir_test_create_error_php . '/../controleur/ControleurUtils.class.php';
-
-echo false == ControleurUtils::addError("test creation erreur", true);
-?>
