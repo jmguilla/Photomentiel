fd6030bc260ada861eba9fd6a562be530bc8d4e8
jmg <guillauj@gmail.com>
2010-10-19 20:26:32 +0200

    try catch pour unlock table dans transactionIDDAO

--- classes/modele/modele_dao/TransactionIDDAO.class.php
+++ classes/modele/modele_dao/TransactionIDDAO.class.php
@@ -31,36 +31,41 @@ class TransactionIDDAO extends DAO {
 		if(!$this->lockTableGet()){
 			ControleurUtils::addError("Impossible de locker la table transactionID pdt get", true);
 		}
-		$query = "select * from transactionID";
-		$this->startTransaction();
-		$tmp = $this->retrieve($query);
-		$transactionID = 1;
-		$query = "insert into transactionID (transactionID) values(1)";
-		if($tmp && $tmp->getNumRows() == 1){
-			foreach($tmp as $row){
-				$transactionID = $row->offsetGet('transactionID');
-				$transactionID++;
-				if($transactionID >= 1000000){
-					$transactionID = 1;
+		try{
+			$query = "select * from transactionID";
+			$this->startTransaction();
+			$tmp = $this->retrieve($query);
+			$transactionID = 1;
+			$query = "insert into transactionID (transactionID) values(1)";
+			if($tmp && $tmp->getNumRows() == 1){
+				foreach($tmp as $row){
+					$transactionID = $row->offsetGet('transactionID');
+					$transactionID++;
+					if($transactionID >= 1000000){
+						$transactionID = 1;
+					}
+					$query = "update transactionID set transactionID = " .
+					mysql_real_escape_string($transactionID);
+					break;
 				}
-				$query = "update transactionID set transactionID = " .
-				mysql_real_escape_string($transactionID);
-				break;
 			}
-		}
-		$tmp = $this->update($query);
-		if($tmp && $this->getAffectedRows() == 1){
-			$this->commit();
-			if(!$this->unlockTable()){
-				ControleurUtils::addError("Impossible de unlocker la table transactionID pdt get sur succes", true);
+			$tmp = $this->update($query);
+			if($tmp && $this->getAffectedRows() == 1){
+				$this->commit();
+				if(!$this->unlockTable()){
+					ControleurUtils::addError("Impossible de unlocker la table transactionID pdt get sur succes", true);
+				}
+				return $transactionID;
+			}else{
+				$this->rollback();
+				if(!$this->unlockTable()){
+					ControleurUtils::addError("Impossible de unlocker la table transactionID pdt get sur echec", true);
+				}
+				return false;
 			}
-			return $transactionID;
-		}else{
-			$this->rollback();
-			if(!$this->unlockTable()){
-				ControleurUtils::addError("Impossible de unlocker la table transactionID pdt get sur echec", true);
-			}
-			return false;
+		}catch(Exception $e){
+			ControleurUtils::addError("Un exception est survenue pendant TransactionID::GET, unlock tables force\n" . $e->getMessage(), true);
+			$this->unlockTable();
 		}
 	}	
 }
