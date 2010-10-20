57736deaf4ea7283d43b544e76047087c9940d52
jmguilla <jean-michel.guillaume@activeeon.com>
2010-10-20 10:26:59 +0200

    newcmd.php

---
+++ administration/AdministrationUtils.class.php
@@ -0,0 +1,17 @@
+<?php
+class AdministrationUtils {
+	public static function sendMailCommandeATraiter(){
+		if($_SERVER['SERVER_ADDR'] != "127.0.0.1"){
+			$headers ='From: "Photomentiel"<contact@photomentiel.fr>'."\n"; 
+     		$headers .='Content-Type: text/plain; charset="utf-8"'."\n"; 
+     		$headers .='Content-Transfer-Encoding: 8bit'; 
+			return mail("jl@photomentiel.fr, jm@photomentiel.fr",
+			"Commande en attente de traitement sur www.photomentiel.fr",
+			"Au moins une commande est en attente de traitement\n".
+			"RDV sur http://admin.photomentiel.fr",
+			$headers
+			);
+		}
+	}	
+}
+?>
---
+++ administration/cron/newcmd.php
@@ -0,0 +1,13 @@
+<?php
+//controle s'il y a des commandes en attente de traitement et envoie un mail les cas où...
+$dir_administration_cron_newcmd_php = dirname(__FILE__);
+include_once $dir_administration_cron_newcmd_php . '/../../classes/modele/Commande.class.php';
+include_once $dir_administration_cron_newcmd_php . '/../AdministrationUtils.class.php';
+
+$commande = Commande::getCommandeEtPhotosDepuisEtat(1);
+//au moins une commande à traiter
+if($commande && count($commande)>0){
+	AdministrationUtils::sendMailCommandeATraiter();
+}
+
+?>
