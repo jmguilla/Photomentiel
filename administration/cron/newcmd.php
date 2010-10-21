a6b5810b3cb9b6a0805d5f4c27c721c7d4f81dd6
jmguilla <jean-michel.guillaume@activeeon.com>
2010-10-21 15:19:05 +0200

    SERVER ajoute a newcmd

--- administration/cron/newcmd.php
+++ administration/cron/newcmd.php
@@ -1,4 +1,5 @@
 <?php
+$_SERVER['SERVER_ADDR'] = "213.186.33.16";
 //controle s'il y a des commandes en attente de traitement et envoie un mail les cas où...
 $dir_administration_cron_newcmd_php = dirname(__FILE__);
 include_once $dir_administration_cron_newcmd_php . '/../../classes/modele/Commande.class.php';
