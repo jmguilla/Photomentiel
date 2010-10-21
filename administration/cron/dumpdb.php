9075459118899b1921a306882b4658ba5caadc64
jmguilla <jean-michel.guillaume@activeeon.com>
2010-10-21 15:51:31 +0200

    cron dump db

---
+++ administration/cron/dumpdb.php
@@ -0,0 +1,22 @@
+<?php
+$_SERVER['SERVER_ADDR'] = "213.186.33.16";
+$dir_administration_dumpdb_php = dirname(__FILE__);
+include_once $dir_administration_dumpdb_php . "/../../classes/Config.php";
+include_once $dir_administration_dumpdb_php . "/../../classes/modele/ModeleUtils.class.php";
+$files = ModeleUtils::getFileFromDirectory("/homez.368/photomen/cgi-bin/sauvegarde/db/");
+if($files && count($files) >= 8){
+	$toDelete = $files[0];
+	$toDeleteTime = filemtime($files[0]);
+	foreach($files as $file){
+		$currentLastModifTime = filemtime($file);
+		if($currentLastModifTime && $currentLastModifTime < $toDeleteTime){
+			$toDeleteTime = $currentLastModifTime;
+			$toDelete = $file;
+		}
+	}
+	unlink($toDelete);
+}
+$date = date("d-m-Y");
+system("mysqldump --host=".DBHOST." --user=".DBUSER." --password=".DBPWD." ".DBPHOTOMENTIEL." > /homez.368/photomen/cgi-bin/sauvegarde/db/save-".$date.".sql");
+
+?>
