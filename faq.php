a2896768fc77fc699487fa1180eafb440172c1c6
unknown <jlscheef@.(none)>
2010-10-26 16:06:22 +0200

    correction sur faq

--- faq.php
+++ faq.php
@@ -29,67 +29,63 @@ include("header.php");
 		<a href="#1"><div class="question">J'ai reçu une carte de votre site, quelle est la marche à suivre ? </div></a>
 		<a href="#11"><div class="question">Un filigrane est visible au milieu des photos, sera-t-il imprimé sur ma photo papier ? </div></a>
 		<a href="#2"><div class="question">Quelle sera la qualité de la photographie reçu ? </div></a>
-		<a href="#3"><div class="question">J'ai selectionné mes photographies mais je ne peux accéder à la page de paiement, pourquoi ? </div></a>
-		<a href="#4"><div class="question">Je n'aime pas donner mon numéro de carte bancaire sur Internet, est ce vraiment sécurisé ? </div></a>
+		<a href="#4"><div class="question">Je n'aime pas donner mon numéro de carte bancaire sur Internet, est-ce vraiment sécurisé ? </div></a>
 		<a href="#5"><div class="question">Puis-je utiliser une photographie achetée sur www.photomentiel.fr à des fins commerciales (prospectus, flyer, carte de voeux, site web, ...) ? </div></a>
 		<a href="#6"><div class="question">Mais ce sont des photographies de moi, je peux quand même les mettre sur mon blog ? </div></a>
-		<a href="#7"><div class="question">Comment rentrer en contact avec le photographe ? </div></a>
+		<a href="#7"><div class="question">Comment contacter le photographe ? </div></a>
 		<a href="#8"><div class="question">Je ne souhaite pas que des photographies de mes enfants soit disponibles sur Internet. </div></a>
 		<a href="#9"><div class="question">Une photographie dont je suis le sujet m'indispose, puis-je la faire retirer ? </div></a>
-		<a href="#10"><div class="question">Mon enfant participe à un tournoi de ping-pong le mois prochain, puis je demander la couverture de cet événement ?</div></a>
+		<a href="#10"><div class="question">Mon enfant participe à un tournoi de ping-pong le mois prochain, puis-je demander la couverture de cet événement ?</div></a>
 		<div class="separator5"></div>
 		<center><hr/></center>
 		<div class="question"><a name="12"></a>La photo papier... des souvenirs qui restent...</div> 
 		<div class="answer">
 		  <span class="start"></span>Vous avez peut être déjà remarqué combien il est difficile de revenir sur des albums numériques dont les dossiers sont peuplés de centaines et centaines de photos dont on ne sait plus par où commencer pour les visionner...<br/>
-		  Photomentiel, au travers de son service, vous propose de sélectionner les meilleurs clichés, les meilleurs souvenirs parmi les centaines d'exemplaires numériques présents et de les ranger soigneusement dans un bel album photo, retraçant tous vos souvenirs.
-		  Quoi de plus convivial que de se retrouver quelques temps plus tard, derrière un bel album, à revivre tous ces moments au travers de ces meilleurs photos.<br/>
-		  Plus besoin de mettre le nez dans des centaines de dossiers et des milliers de photos, Photomentiel, c'est la possibilité de vous offrir simplement les meilleurs souvenirs en ouvrant un simple album photos qui retrace votre vie en image. Un album photo, c'est le livre de votre vie.
+		  Photomentiel, vous propose de sélectionner les meilleurs clichés, les meilleurs souvenirs parmi les centaines d'exemplaires numériques présents et de les ranger soigneusement dans un bel album photo, retraçant tous vos souvenirs.
+		  Quoi de plus convivial que de se retrouver quelques temps plus tard, devant un bel album, pour revivre tous ces moments au travers de ces meilleurs photos.<br/>
+		  Un album photo, c'est le livre de votre vie.
 		</div>
 		<div class="question"><a name="1"></a>J'ai reçu une carte de votre site, quelle est la marche à suivre ?</div> 
 		<div class="answer">
-		  <span class="start"></span>Rendez vous sur la <a href="/index.php">page d'accueil</a> et saisissez le code inscrit en bas de la carte qui vous a été remise. Vous accèdez alors aux photographies prises lors de l'événement auquel vous avez participé, faites votre sélection et laissez vous guider jusqu'à la phase de paiement. Vous recevrez ensuite les photographies sélectionnées par les services postaux.
+		  <span class="start"></span>Rendez vous sur la <a href="/index.php">page d'accueil</a> et saisissez le code inscrit en bas de la carte qui vous a été remise. Vous accédez alors aux photographies prises lors de l'événement auquel vous avez participé, faites votre sélection et laissez vous guider jusqu'à la phase de paiement. Vous recevrez ensuite les photographies sélectionnées par les services postaux.
 		</div>
 		<div class="question"><a name="11"></a>Un filigrane est visible au milieu des photos, sera-t-il imprimé sur ma photo papier ? </div> 
 		<div class="answer">
-		  <span class="start">Evidemment non, ce filigrane est présent afin de protéger les droits d'auteurs des photographes.</span>
+		  <span class="start">Non, ce filigrane est présent afin de protéger les droits d'auteurs des photographes.</span>
 		</div>
-		<div class="question"><a name="2"></a>Quelle sera la qualité de la photographie reçu ?</div>
+		<div class="question"><a name="2"></a>Quelle sera la qualité de la photographie reçue ?</div>
 		<div class="answer">
-		  <span class="start"></span>Nous incluons un système automatique qui vérifie que la résolution de la photographie sera suffisante pour les formats proposés en accord avec les recommandations de notre tireur de photographie. Chaque album publié est vérifié manuellement par l'un de nos techniciens et les photographies présentant des défauts importants de lumière ou de cadrage sont exclues par nos soins. Les photographies visibles sur le site (hormis les miniatures) sont de qualité nettement inférieure au format original pour des raisons de transfert et temps de chargement. Néanmoins elles vous permettent d'avoir une bonne idée du rendu final de la photographie, la réduction de qualité que nous effectuons sur les photos mise en ligne n'influe pas sur le cadre, la composition, ou bien l'éclairage de la photographie. Nous proposons en plus, un système de notation des photographes basé sur VOS appréciations, vous saurez alors à quel point vous pouvez faire confiance à ce photographe.
-		  La note d'un photographe est visible lors de la visite d'une gallerie dans le panneau de gauche sous la forme d'étoile, pour en savoir plus sur le système de notation, rendez la page LIENnotation/LIEN.
+		  <span class="start"></span>Nous incluons un système automatique qui vérifie que la résolution de la photographie sera suffisante pour les formats proposés en accord avec les recommandations d'un professionnel du développement de photo. Chaque album publié est vérifié manuellement par l'un de nos techniciens et les photographies présentant des défauts importants de lumière ou de cadrage sont exclues par nos soins. Les photographies visibles sur le site (hormis les miniatures) sont de qualité nettement inférieure au format original pour des raisons de transfert et temps de chargement. Néanmoins elles vous permettent d'avoir une bonne idée du rendu final de la photographie, la réduction de qualité que nous effectuons sur les photos mise en ligne n'influe pas sur le cadrage, la composition, ou bien l'éclairage de la photographie. Nous proposons également, un système de notation des photographes, vous saurez alors à quel point vous pouvez faire confiance à ce photographe.
+		  La note d'un photographe est visible lors de la visite d'une galerie dans le panneau de gauche sous la forme d'étoiles.
 		</div>
-		<div class="question"><a name="3"></a>J'ai selectionné mes photographies mais je ne peux accéder à la page de paiement, pourquoi ?</div>
+		<div class="question"><a name="4"></a>Je n'aime pas donner mon numéro de carte bancaire sur Internet, est-ce vraiment sécurisé ?</div>
 		<div class="answer">
-		  <span class="start"></span>Avez vous pensé à sélectionné les formats voulus pour chaque photographies ? Selon la résolution des photographies originales, un ensemble de formats vous serons proposés ainsi que plusieurs supports tels que l'impression sur toile, sur support acier, le livre photo, ... N'hésitez pas à varier vous en avez la possibilité !
-		</div>
-		<div class="question"><a name="4"></a>Je n'aime pas donner mon numéro de carte bancaire sur Internet, est ce vraiment sécurisé ?</div>
-		<div class="answer">
-			<span class="start"></span>Le système de paiement en ligne que nous utilisons est complètement indépendant de notre infrastructure, il nous est fourni par le Crédit Agricole avec la solution e-transaction, de ce fait la sécurité du paiement est assurée par une des plus grandes banques françaises, à aucun moment nous n'avons accés à vos informations bancaires. De plus l'ensemble des données que vous échangez avec le Crédit Agricole se font au travers d'un lien de communication chiffré basé sur SSL/TLS qui présente à ce jour une solution de sécurité prouvée et éprouvée ! Le paiement en ligne ne présente plus de danger à l'heure actuelle dés lors que vous étes en relation avec un organisme sérieux, encore une fois, ce n'est pas www.photomentiel.fr mais bien la banque française : Le Crédit Agricole qui vous assure une transaction sécurisée.
+			<span class="start"></span>Le système de paiement en ligne que nous utilisons est complètement indépendant de notre infrastructure, il nous est fourni par le Crédit Agricole avec la solution e-transaction, de ce fait la sécurité du paiement est assurée par une des plus grandes banques françaises, à aucun moment nous n'avons accès à vos informations bancaires. De plus l'ensemble des données que vous échangez avec l'organisme bancaire se fait au travers d'un lien de communication chiffré basé sur SSL/TLS qui présente à ce jour une solution de sécurité prouvée et éprouvée ! Le paiement en ligne ne présente plus de danger à l'heure actuelle dès lors que vous êtes en relation avec un organisme sérieux, encore une fois, ce n'est pas www.photomentiel.fr mais bien la banque française : Le Crédit Agricole qui vous assure une transaction sécurisée.
 		</div>
 		  <div class="question"><a name="5"></a>Puis-je utiliser une photographie achetée sur www.photomentiel.fr à des fins commerciales (prospectus, flyer, carte de voeux, site web, ...) ?</div>
 		<div class="answer">
-		  <span class="start"></span>Non, pas en l'état. En utilisant les services de www.photomentiel.fr vous reconnaissez accepter les <a href=cgu.php>Conditions d'utilisation</a> qui stipulent que les photographies mises à disposition par www.photomentiel.fr le sont pour un ordre privé et familiale uniquement. Nous vous invitons à rentrer en contact avec le photographe qui detient les droits de la photographie afin de convenir d'un accord.
+		  <span class="start"></span>Non, pas en l'état. En utilisant les services de www.photomentiel.fr vous reconnaissez accepter les <a href=cgu.php>Conditions d'utilisation</a> qui stipulent que les photographies mises à disposition par www.photomentiel.fr le sont à des fins privées et familiales uniquement. Nous vous invitons à entrer en contact avec le photographe qui détient les droits de la photographie afin de convenir d'un accord.
 		</div>
 		  <div class="question"><a name="6"></a>Mais ce sont des photographies de moi, je peux quand même les mettre sur mon blog ?</div>
 		<div class="answer">
-		  <span class="start"></span>Même si vous êtes le sujet de la photographie, vous n'en detenez pas la propriété intellectuelle. Dès lors que votre blog à une visée privée et/ou familiale, vous êtes autorisé à utiliser la photographie. Dans le cas contraire merci de valider l'utilisation avec le photographe.
+		  <span class="start"></span>Même si vous êtes le sujet de la photographie, vous n'en détenez pas la propriété intellectuelle. Si votre blog a un caractère privé et/ou familiale, alors vous pouvez utiliser la photographie, sinon devez convenir d'un accord avec le photographe.
 		</div>
-		<div class="question"><a name="7"></a>Comment rentrer en contact avec le photographe ?</div>
+		<div class="question"><a name="7"></a>Comment contacter le photographe ?</div>
 		<div class="answer">
-		  <span class="start"></span>Chaque photographe dispose d'une fiche de profil accessible depuis l'album via le panneau de gauche. Vous trouverez alors toutes les informations que le photographe décide de partager, dans le cas où aucune information n'est présente, contactez nous via la page <a href=contact.php> contact </a>, nous nous chargerons de transmettre votre requête. 
+		  <span class="start"></span>Chaque photographe dispose d'une fiche de profil accessible depuis l'album en cliquant sur son nom. Vous trouverez alors toutes les informations que le photographe décide de partager, dans le cas où aucune information n'est présente, contactez nous via la page <a href=contact.php> contact </a>, nous nous chargerons de lui transmettre votre requête.<br/>
+		Dans le cas où vous avez reçu une carte Photomentiel, vous y trouverez très certainement l'Email du photographe.
 		</div>
 		<div class="question"><a name="8"></a>Je ne souhaite pas que des photographies de mes enfants soient disponibles sur Internet.</div>
 		<div class="answer">
-			<span class="start"></span>Bien sur, nous vous comprenons tout à fait, les albums que nous mettons en ligne ne sont pas accessibles aux moteurs de recherche et ne peuvent donc pas être référencés de manière automatique. L'accès à un album se fait au moyen d'un identifiant alpha-numérique de 8 caractères générés aléatoirement, autant dire qu'il y a autant de chance de tomber dessus par hasard que d'être percuté par un asteroïde le jour de son mariage ! Nos conditions d'utilisations stipulent, qu'il est interdit de diffuser publiquement cet identifiant et nous feront valoir cette interdiction à chaque fois que nous constaterons un manquement à cette règle.
+			<span class="start"></span>Bien sûr, nous vous comprenons tout à fait, les albums privés que nous mettons en ligne ne sont pas accessibles par les moteurs de recherche et ne peuvent donc pas être référencés de manière automatique. L'accès à un album se fait au moyen d'un identifiant alpha-numérique de 8 caractères générés aléatoirement, autant dire qu'il y a autant de chance de tomber dessus par hasard que d'être percuté par une astéroïde le jour de son mariage ! Nos conditions d'utilisations stipulent, qu'il est interdit de diffuser publiquement cet identifiant et nous ferons valoir cette interdiction à chaque fois que nous constaterons un manquement à cette règle.
 		</div>
 		<div class="question"><a name="9"></a>Une photographie dont je suis le sujet m'indispose, puis-je la faire retirer ?</div>
 		<div class="answer">
-		  <span class="start"></span>Conformément aux lois informatiques, vous êtes autorisé à demander le retrait de photographies sur lesquelles vous apparaissez. Pour cela rendez vous à la rubrique <a href=privacy.php> vie privée</a> et remplissez le formulaire. Vous devez impérativement être le sujet principal de la photographie ou bien nettement visible pour que votre demande soit prise en compte, merci de votre compréhension. 
+		  <span class="start"></span>Conformément aux lois informatiques et libertés, vous êtes en droit de demander le retrait de photographies sur lesquelles vous apparaissez. Pour cela rendez vous à la rubrique <a href=privacy.php> vie privée</a> et remplissez le formulaire. Vous devez impérativement être le sujet principal de la photographie ou bien nettement visible pour que votre demande soit prise en compte, merci de votre compréhension. 
 		</div>
-		  <div class="question"><a name="10"></a>Mon enfant participe à un tournoi de ping-pong le mois prochain, puis je demander la couverture de cet événement ?</div>
+		  <div class="question"><a name="10"></a>Mon enfant participe à un tournoi de ping-pong le mois prochain, puis-je demander la couverture de cet événement ?</div>
 		<div class="answer">
-		  <span class="start"></span> Oui ! Notre but est de mettre en relation les particuliers désireux de profiter des services de photographes professionnels et les-dits photographes. Pour cela, rendez vous dans la section <a href=events.php> calendrier </a> et, après avoir vérifié que votre événement n'y figure pas déjà, notifiez le au moyen du bouton "Ajouter un événement". Dès lors l'ensemble des photographes sera notifié de la date et du lieu de votre événement. Au moment où l'un d'entre eux validera sa présence sur cet événement, vous serez notifié par e-mail.
+		  <span class="start"></span> Oui ! Notre but est de mettre en relation les particuliers désireux de profiter des services de photographes professionnels et les-dits photographes. Pour cela, rendez vous dans la section <a href=events.php> calendrier </a> et, après avoir vérifié que votre événement n'y figure pas déjà, notifiez le au moyen du bouton "Ajouter un événement". Dès lors l'ensemble des photographes sera informé de la date et du lieu de votre événement.
 		</div>
 		<br/>
 	</div>
