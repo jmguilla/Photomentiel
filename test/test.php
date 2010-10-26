a2284196842993d35da7d4303befc5c3a70d1328
jmg <guillauj@gmail.com>
2010-10-26 19:46:27 +0200

    cleanup

--- test/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once '../classes/controleur/ControleurUtils.class.php';
-include_once '../classes/modele/Utilisateur.class.php';
-$user = new Utilisateur();
-$user->setEmail("guillauj@gmail.com");
-echo (false == ControleurUtils::sendMailEtPDF($user, "sujet au hasard", "contenu au hasard", "/homez.368/photomen/cgi-bin/5_Facturier.pdf"));
